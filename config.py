b59021bdfb50bfad5a5acf06e7a3c747e6115b10
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-12-25 19:00:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL", "https://api.together.xyz/v1/chat/completions")
 
-SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/BABY09_WORLD")
-UPDATES_LINK = os.getenv("UPDATES_LINK", "https://t.me/BABY09_UPDATES")
+SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/Exampurrs")
+UPDATES_LINK = os.getenv("UPDATES_LINK", "https://t.me/APNA_SYSTEM")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "")
