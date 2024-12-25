e16f139bb8d34a3c1c7e3c7a0823138c2ba1a7f5
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-12-25 17:52:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 API_KEY = os.getenv("API_KEY")
-BASE_URL = os.getenv("BASE_URL", "https://api.openai.com/v1/chat/completions")
+BASE_URL = os.getenv("BASE_URL", "https://api.together.xyz/v1/chat/completions")
 
 SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/BABY09_WORLD")
 UPDATES_LINK = os.getenv("UPDATES_LINK", "https://t.me/BABY09_UPDATES")
